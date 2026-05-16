bde48eb414f88c62457bf2e5a62920fcb25faeba
israkhan <isranasir.ns2@gmail.com>
2026-05-16 17:58:59 +0500

    both approach added

--- modules/modulescalled.js
+++ modules/modulescalled.js
@@ -1,4 +1,5 @@
 
 const addition=require('./modules.js');
-
+ //const {add}=require('./modules'); 
+ // add(2,3) would also work if we use destructuring assignment in the require statement
 console.log(addition.add(2,3));
